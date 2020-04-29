853a588816e1226d50b7c0379b4c420c1a0949ea
c0320l1 <vuhongson2614@gmail.com>
2020-04-29 09:45:20 +0700

    TINH Nang

---
+++ js-CASESTUDY.js
@@ -0,0 +1,16 @@
+function submit() {
+        let x = document.getElementById("product");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+function submits() {
+    let x = document.getElementById("products");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
