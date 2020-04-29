a11306341f9e7609ec8ed34d8b9c4a19d66b3dcf
c0320l1 <vuhongson2614@gmail.com>
2020-04-29 15:38:40 +0700

    hoan thien

--- js-CASESTUDY.js
+++ js-CASESTUDY.js
@@ -1,16 +1,53 @@
 function submit() {
-        let x = document.getElementById("product");
-    if (x.style.display === "none") {
-        x.style.display = "block";
-    } else {
-        x.style.display = "none";
+   let product;
+   let gmail = "@GMAIL.COM"
+    product = prompt("GMAIL:")
+    if (product == null){
+        alert("NO WAY")
+    }if (product ==! gmail){
+        alert("not gmail")
+    }else {
+        alert("THANKS YOU")
+    }
+}
+function BUYSE() {
+    let i;
+    i = prompt("Bạn có chắc chắn không?")
+    alert("cảm ơn vì đã tin tưởng")
+}
+function submitsss() {
+    let product;
+    let gmail = "@GMAIL.COM"
+    product = prompt("GMAIL:")
+    if (product == null){
+        alert("NO WAY")
+    }if (product ==! gmail){
+        alert("not gmail")
+    }else {
+        alert("THANKS YOU")
     }
 }
 function submits() {
-    let x = document.getElementById("products");
-    if (x.style.display === "none") {
-        x.style.display = "block";
-    } else {
-        x.style.display = "none";
+    let product;
+    let gmail = "@GMAIL.COM"
+    product = prompt("GMAIL:")
+    if (product == null){
+        alert("NO WAY")
+    }if (product ==! gmail){
+        alert("not gmail")
+    }else {
+        alert("THANKS YOU")
+    }
+}
+function submitss() {
+    let product;
+    let gmail = "@GMAIL.COM"
+    product = prompt("GMAIL:")
+    if (product == null){
+        alert("NO WAY")
+    }if (product ==! gmail){
+        alert("not gmail")
+    }else {
+        alert("THANKS YOU")
     }
 }
