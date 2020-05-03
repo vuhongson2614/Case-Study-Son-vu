06b5ef03cf8be2e16a4f50044009cbe265ae04ae
c0320l1 <vuhongson2614@gmail.com>
2020-05-03 22:11:07 +0700

    hoan thien

--- js-CASESTUDY.js
+++ js-CASESTUDY.js
@@ -1,53 +1,49 @@
-function submit() {
-   let product;
-   let gmail = "@GMAIL.COM"
-    product = prompt("GMAIL:")
-    if (product == null){
-        alert("NO WAY")
-    }if (product ==! gmail){
-        alert("not gmail")
-    }else {
-        alert("THANKS YOU")
+let Nameproduct = document.getElementById("full-name");
+let amount = document.getElementById("another-name");
+let dob = document.getElementById("birth");
+let gender = document.getElementsByName('gender');
+let address = document.getElementById("address");
+let nationality = document.getElementById("nationality");
+let height = document.getElementById("height");
+let avatar = document.getElementById("avatar");
+let idolSelected = -1;
+
+function save() {
+    let idol = new Idol(1, Nameproduct.value, amount.value, dob.value, getGender(), address.value, nationality.value, height.value, avatar.value);
+    idolManage.addIdol(idol);
+    saveData();
+    idolManage.display();
+}
+function deleteIdol(index) {
+    idolManage.deleteIdol(index);
+    saveData();
+    idolManage.display();
+}
+
+function editIdol(index) {
+    let id = index - 1;
+    Nameproduct.value = idolManage.idols[id].Nameproduct;
+    amount.value = idolManage.idols[id].amount;
+    dob.value = idolManage.idols[id].birth;
+    address.value = idolManage.idols[id].address;
+    nationality.value = idolManage.idols[id].nationality;
+    height.value = idolManage.idols[id].height;
+    avatar.value = idolManage.idols[id].avatar;
+    idolSelected = id;
+}
+loadData();
+idolManage.display();
+function show() {
+    if(document.getElementById('benefits').style.display=='none') {
+        document.getElementById('benefits').style.display='block';
     }
+    return false;
 }
-function BUYSE() {
-    let i;
-    i = prompt("Bạn có chắc chắn không?")
-    alert("cảm ơn vì đã tin tưởng")
-}
-function submitsss() {
-    let product;
-    let gmail = "@GMAIL.COM"
-    product = prompt("GMAIL:")
-    if (product == null){
-        alert("NO WAY")
-    }if (product ==! gmail){
-        alert("not gmail")
-    }else {
-        alert("THANKS YOU")
+function hide() {
+    if(document.getElementById('benefits').style.display=='block') {
+        document.getElementById('benefits').style.display='none';
     }
+    return false;
 }
-function submits() {
-    let product;
-    let gmail = "@GMAIL.COM"
-    product = prompt("GMAIL:")
-    if (product == null){
-        alert("NO WAY")
-    }if (product ==! gmail){
-        alert("not gmail")
-    }else {
-        alert("THANKS YOU")
-    }
-}
-function submitss() {
-    let product;
-    let gmail = "@GMAIL.COM"
-    product = prompt("GMAIL:")
-    if (product == null){
-        alert("NO WAY")
-    }if (product ==! gmail){
-        alert("not gmail")
-    }else {
-        alert("THANKS YOU")
-    }
-}
+
+
